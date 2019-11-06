05a8d04c054d10389c8f60a5d8c85d4a4ca47017
nerea <nlabanderafe18dw@ikzubirimanteo.com>
2019-11-06 08:12:29 +0100

    4.3 a terminado

--- app/Http/Controllers/SaludoController.php
+++ app/Http/Controllers/SaludoController.php
@@ -9,8 +9,8 @@ class SaludoController extends Controller
     function saluda(){
     	return view ('saludo');
     }
-    function saludaNombre($nombre,$apellido){
-    	return view ('saludonombre',['nombre'=>$nombre,'apellido'=>$apellido]);
+    function saludaNombre(){
+    	return view ('saludonombre',['nombre'=>request('nombre'),'apellido'=>request('apellido')]);
     }
     function saludaColor($nombre,$color){
     	return view ('saludocolor',['nombre'=>$nombre,'color'=>$color]);
